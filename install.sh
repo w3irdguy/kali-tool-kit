ccfff4887a95f3443097a433784f1b8f1dc5045a
z4ck <127699732+z4kc@users.noreply.github.com>
2024-04-01 19:06:49 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,73 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install git -y
+sudo apt install airgeddon -y
+sudo apt install proxychains4 -y
+sudo apt install tor -y
+wget https://download.virtualbox.org/virtualbox/7.0.14/VirtualBox-7.0.14-161095-Linux_amd64.run
+chmod +x VirtualBox-7.0.14-161095-Linux_amd64.run
+sudo ./VirtualBox-7.0.14-161095-Linux_amd64.run
+git clone https://github.com/tegal1337/CiLocks
+sudo apt install virtualbox-qt -y
+sudo apt install code-oss -y
+sudo apt install rkhunter -y
+sudo apt install metasploit-framework -y
+sudo apt install dirb -y
+sudo apt install hydra -y
+sudo apt install nmap -y
+sudo apt install zmap -y
+sudo apt install recon-ng -y
+sudo apt install john -y
+sudo apt install armitage -y
+sudo apt install aircrack-ng -y
+sudo apt install wifite -y
+sudo apt install wpscan -y
+sudo apt install hashcat -y
+sudo apt install sherlock -y
+sudo apt install spiderfoot -y
+sudo apt install legion -y
+sudo apt install maltego -y
+sudo apt install beef-xss -y
+sudo apt install yara -y
+sudo apt install fcrackzip -y
+sudo apt install sqlmap -y
+sudo apt install tcpdump -y
+sudo apt install ettercap -y
+sudo apt install burpsuite -y
+sudo apt install ettercap -y
+sudo apt install wireshark -y
+sudo apt install binwalk -y
+sudo apt install wifiphisher -y
+sudo apt install netdiscover -y
+sudo apt install firewalk -y
+sudo apt install nikto -y
+sudo apt install fping -y
+sudo apt install arpwatch -y
+sudo apt install scapy -y
+sudo apt install bed -y
+sudo apt install doona -y 
+sudo apt install cisco-ocs -y
+sudo apt install dotdotpwn -y
+sudo apt install sfuzz -y
+sudo apt install sqlsus -y
+sudo apt install tnscmd10g -y
+sudo apt install sidguesser -y
+sudo apt install siparmyknife -y
+sudo apt install arpwatch -y
+sudo apt install bulk-extractor -y
+sudo apt install chntpw -y
+sudo apt install dumpzilla -y
+sudo apt install foremost -y
+sudo apt install p0f -y
+sudo apt install pdf-parser -y
+sudo apt install pdfid -y
+sudo apt install dc3dd -y
+sudo apt install bluelog -y
+sudo apt install bully -y
+sudo apt install crackle -y
+sudo apt install hcxtools -y
+sudo apt install mana -y
+sudo apt install ipv6-toolkit -y
+sudo apt install termineter -y
+sudo apt install iaxflood -y
+sudo apt install rtpflood -y
