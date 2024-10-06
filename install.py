ab1870dcfab0a52a1ede0bdea3295a9c61ee43a8
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-10-06 16:42:12 -0300

    Update install.py

--- install.py
+++ install.py
@@ -5,6 +5,7 @@ def root():
   if rootcheck == 0:
     with open('kali-full.txt', 'r') as file:
       for line in file.readlines():
+        print(f"installing {line}...")
         os.system(f"sudo apt install {line} -y")
   else:
     print("you need \033[0;91m root\033[0m to continue")
