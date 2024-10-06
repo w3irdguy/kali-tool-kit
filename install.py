5d6140bcc5dcd4cdde81a3ee0d3d670f0b060ddf
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-10-06 16:38:53 -0300

    Create install.py

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+
+def root():
+  rootcheck = os.geteuid()
+  if rootcheck == 0:
+    with open('kali-full.txt', 'r') as file:
+      for line in file.readlines():
+        os.system(f"sudo apt install {line} -y")
+  else:
+    print("you need \033[0;91m; root\033[0m")
+      
